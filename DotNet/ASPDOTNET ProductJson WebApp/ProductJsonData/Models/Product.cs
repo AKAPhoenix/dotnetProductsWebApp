856eb4ff42e3b2a1a8ee2b951c478601773ebf3f
Akanowo Ekarika <texcellens@gmail.com>
2022-02-20 22:56:11 +0100

    First Commit

---
+++ DotNet/ASPDOTNET ProductJson WebApp/ProductJsonData/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace ProductJsonData.Models
+{
+    public class Product
+    {
+        public Product(string name,int price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+        public string name { get; set; }
+        public int price { get; set; }
+    }
+}
